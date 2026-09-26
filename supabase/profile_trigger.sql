2a9f76761df5d18611090d22dd7bb20ffc7ceb1e
Catalin-Ion Besleaga <c_beslea@live.concordia.ca>
2026-09-26 16:04:16 -0400

    feat: automatically create profiles for new users

---
+++ supabase/profile_trigger.sql
@@ -0,0 +1,18 @@
+-- Automatically create a profile for each new Auth user.
+create function public.handle_new_user()
+returns trigger
+language plpgsql
+security definer
+set search_path = ''
+as $$
+begin
+  insert into public.profiles (id, full_name)
+  values (new.id, new.raw_user_meta_data ->> 'full_name');
+  return new;
+end;
+$$;
+
+create trigger on_auth_user_created
+after insert on auth.users
+for each row
+execute function public.handle_new_user();
